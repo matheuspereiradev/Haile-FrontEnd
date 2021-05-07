e8041f2a4c213571fa48d7d10739969fd1506cd1
matheus <matheuslima20111997@gmail.com>
2021-05-06 21:23:38 -0300

    melhorias no layout

---
+++ src/components/filters/index.tsx
@@ -0,0 +1,19 @@
+import styles from "./style.module.scss";
+
+export function Filters() {
+
+  return (
+    <div className={styles.filterContainer}>
+        <div className={styles.cardBody}>
+          <div className={styles.cardTitle}>
+              aaa
+          </div>
+          <div className={styles.cardValue}>
+              aaaa
+          </div>
+          <button className={styles.btnBuy}>Filtrar</button>
+        </div>
+    </div>
+  );
+
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps } from 'next';
 import { Banner } from '../components/banner';
 import { CardHouse } from '../components/cardHouse'
+import { Filters } from '../components/filters';
 import { RectangleCard } from '../components/rectangleCard';
 import { api } from '../services/axiosbackend';
 import styles from '../styles/index.module.scss'
@@ -28,28 +29,37 @@ export default function Home({properties}:CardInfo) {
   return (
 <>
     <Banner/>
-    <div className={styles.table}>
-      {
-        properties.map(house=>{
-          return(
-              <RectangleCard 
-                id={house.id} 
-                state={house.state} 
-                city={house.city} 
-                amountValue={house.amountValue} 
-                imageLink={house.imageLink} 
-                key={house.id} 
-                street={house.street} 
-                number={house.number}
-                district={house.district}
-                details={house.details}
-                adviser={house.adviser}
-                contractType={house.contractType}
-              />
-            )
-        })
-      }
-    </div>
+    <main className={styles.contentMain}>
+      <div className={styles.leftArea}>
+        <div className={styles.componentFilters}>
+          <Filters/>
+        </div>
+          
+      </div>
+      
+      <div className={styles.table}>
+        {
+          properties.map(house=>{
+            return(
+                <RectangleCard 
+                  id={house.id} 
+                  state={house.state} 
+                  city={house.city} 
+                  amountValue={house.amountValue} 
+                  imageLink={house.imageLink} 
+                  key={house.id} 
+                  street={house.street} 
+                  number={house.number}
+                  district={house.district}
+                  details={house.details}
+                  adviser={house.adviser}
+                  contractType={house.contractType}
+                />
+              )
+          })
+        }
+      </div>
+    </main>
     
  </>     
   )
