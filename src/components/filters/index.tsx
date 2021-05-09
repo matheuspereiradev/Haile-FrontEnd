5e9a69ec33cdc3fbd594e49c5985a972d185a2dd
matheus <matheuslima20111997@gmail.com>
2021-05-09 13:23:25 -0300

    correção dolayout

--- src/components/filters/index.tsx
+++ src/components/filters/index.tsx
@@ -1,43 +1,72 @@
 import styles from "./style.module.scss";
-import {FaFilter, FaLess, FaMinus} from "react-icons/fa"
+import { FaFilter, FaLess, FaMinus, FaPlus } from "react-icons/fa"
+import { useState } from "react";
 
 export function Filters() {
 
+  const [visibleContractType, setVisibleContractType] = useState(true)
+  const [visiblePropertType, setvisiblePropertType] = useState(true)
+
   return (
     <div className={styles.filterContainer}>
-        <div className={styles.cardBody}>
-          <div className={styles.cardTitle}>
-            <span><FaFilter/> Filtros</span>
-          </div>
-          <div className={styles.cardValue}>
-            <div className={styles.header}>
-              <small>Tipos de contratos</small>
-              <i><FaMinus/></i>
-            </div>
-            <div className={styles.value}>
-              <input type="checkbox" value="Aluguel"/>
-              <label>Aluguel</label>
-            </div>
-            <div className={styles.value}>
-              <input type="checkbox" value="Compra"/>
-              <label>Compra</label>
-            </div>
-            
-            <div className={styles.header}>
-              <small>Tipos de contratos</small>
-              <i><FaMinus/></i>
-            </div>
-            <div className={styles.value}>
-              <input type="checkbox" value="Aluguel"/>
-              <label>Aluguel</label>
-            </div>
-            <div className={styles.value}>
-              <input type="checkbox" value="Compra"/>
-              <label>Compra</label>
-            </div>
-          </div>
-
+      <div className={styles.cardBody}>
+        <div className={styles.cardTitle}>
+          <span><FaFilter /> Filtros</span>
         </div>
+        <div className={styles.cardContent}>
+          <div>
+            <div className={styles.header} onClick={() => { setVisibleContractType(!visibleContractType) }}>
+              <small>Tipos de contratos</small>
+              {visibleContractType ? (
+                <i><FaMinus /></i>
+              ) : (
+                <i><FaPlus /></i>
+              )}
+
+            </div>
+            {visibleContractType && (
+              <div>
+                <div className={styles.value}>
+                  <input type="checkbox" value="Aluguel" />
+                  <label>Aluguel</label>
+                </div>
+                <div className={styles.value}>
+                  <input type="checkbox" value="Compra" />
+                  <label>Compra</label>
+                </div>
+              </div>
+            )}
+
+          </div>
+          <div>
+            <div className={styles.header} onClick={() => { setvisiblePropertType(!visiblePropertType) }}>
+              <small>Tipos de casa</small>
+              {visiblePropertType ? (
+                <i><FaMinus /></i>
+              ) : (
+                <i><FaPlus /></i>
+              )}
+            </div>
+
+            {visiblePropertType && (
+              <div>
+                <div className={styles.value}>
+                  <input type="checkbox" value="casa" />
+                  <label>Casa</label>
+                </div>
+                <div className={styles.value}>
+                  <input type="checkbox" value="apartamento" />
+                  <label>Apartamento</label>
+                </div>
+                <div className={styles.value}>
+                  <input type="checkbox" value="sobrado" />
+                  <label>Sobrado</label>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 
