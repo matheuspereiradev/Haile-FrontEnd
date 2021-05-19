23b4262119b602d9abb6e70663d37a64af7551ee
matheus <matheuslima20111997@gmail.com>
2021-05-19 20:02:46 -0300

    index page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,18 @@
 import { Header } from '../components/header'
-import '../styles/global.scss'
-import styles from '../styles/app.module.scss'
-import Head from 'next/head'
+import '../styles/global.scss';
+import styles from '../styles/app.module.scss';
+import { HouseContextProvider } from './contexts/HousesContext';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <title>Sobre</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="copyright" content="HAILE" />
-        <meta name="language" content="PT-BR" />
-        <meta name="author" content="matheuspereiradev, matheuslima20111997@gmail.com"></meta>
-      </Head>
       <div className={styles.appWrapper}>
-        <main>
-          <Header />
-          <Component {...pageProps} />
-        </main>
+        <HouseContextProvider>
+          <main>
+            <Header />
+            <Component {...pageProps} />
+          </main>
+        </HouseContextProvider>
       </div>
     </>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,55 +1,62 @@
 import { GetStaticProps } from 'next';
-import { Banner } from '../components/banner';
 import { BannerHome } from '../components/bannerHome';
-import { CardHouse } from '../components/cardHouse'
 import { Filters } from '../components/filters';
 import { RectangleCard } from '../components/rectangleCard';
 import { api } from '../services/axiosbackend';
-import styles from '../styles/index.module.scss'
+import styles from '../styles/index.module.scss';
+import Head from 'next/head';
 
-interface propertyInfo{
-  id:string,
-    city:string,
-    state:string,
-    imageLink:string,
-    amountValue:number,
-    contractType:string,
-    adviser:string,
-    street:string,
-    district:string,
-    number:string,
-    details:string
+interface propertyInfo {
+  id: string,
+  city: string,
+  state: string,
+  imageLink: string,
+  amountValue: number,
+  contractType: string,
+  adviser: string,
+  street: string,
+  district: string,
+  number: string,
+  details: string
 }
 
 interface CardInfo {
-  properties:Array<propertyInfo>
+  properties: Array<propertyInfo>
 }
 
 
-export default function Home({properties}:CardInfo) {
+export default function Home({ properties }: CardInfo) {
   return (
-<>
-    <BannerHome/>
-    <main className={styles.contentMain}>
-      <div className={styles.leftArea}>
-        <div className={styles.componentFilters}>
-          <Filters/>
+    <>
+      <Head>
+        <title>HAILE - Home</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="copyright" content="HAILE" />
+        <meta name="language" content="PT-BR" />
+        <meta name="author" content="matheuspereiradev, matheuslima20111997@gmail.com"/>
+        <meta name="keywords" content="imoveis,imóveis,casa,comprar,haile,corretor,corretores,apartamentos,aluguel,arrendatar,casas"/>
+      </Head>
+      <BannerHome/>
+      <main className={styles.contentMain}>
+        <div className={styles.leftArea}>
+          <div className={styles.componentFilters}>
+            <Filters/>
+          </div>
+
         </div>
-          
-      </div>
-      
-      <div className={styles.table}>
-        {
-          properties.map(house=>{
-            return(
-                <RectangleCard 
-                  id={house.id} 
-                  state={house.state} 
-                  city={house.city} 
-                  amountValue={house.amountValue} 
-                  imageLink={house.imageLink} 
-                  key={house.id} 
-                  street={house.street} 
+
+        <div className={styles.table}>
+          {
+            properties.map(house => {
+              return (
+                <RectangleCard
+                  id={house.id}
+                  state={house.state}
+                  city={house.city}
+                  amountValue={house.amountValue}
+                  imageLink={house.imageLink}
+                  key={house.id}
+                  street={house.street}
                   number={house.number}
                   district={house.district}
                   details={house.details}
@@ -57,42 +64,42 @@ export default function Home({properties}:CardInfo) {
                   contractType={house.contractType}
                 />
               )
-          })
-        }
-      </div>
-    </main>
-    
- </>     
+            })
+          }
+        </div>
+      </main>
+
+    </>
   )
 }
 
-export const getStaticProps:GetStaticProps = async ()=>{
-  
-  const {data} = await api.get('property');
+export const getStaticProps: GetStaticProps = async () => {
 
-  const properties:propertyInfo = data.map(property=>{
-    return{
-        id:property.id,
-        city:property.city.name,
-        state:property.city.uf,
-        amountValue:Number(property.amountValue).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }),
-        imageLink:"https://cdn.pixabay.com/photo/2016/06/24/10/47/house-1477041_1280.jpg",
-        street: property.street,
-        number:property.houseNumber,
-        district:property.district,
-        details:property.note,
-        adviser:property.advertiser.name,
-        contractType:property.contractType.name
+  const { data } = await api.get('property');
+
+  const properties: propertyInfo = data.map(property => {
+    return {
+      id: property.id,
+      city: property.city.name,
+      state: property.city.uf,
+      amountValue: Number(property.amountValue).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }),
+      imageLink: "https://cdn.pixabay.com/photo/2016/06/24/10/47/house-1477041_1280.jpg",
+      street: property.street,
+      number: property.houseNumber,
+      district: property.district,
+      details: property.note,
+      adviser: property.advertiser.name,
+      contractType: property.contractType.name
     }
-    
+
   })
-  
-  return{
-    props:{
+
+  return {
+    props: {
       properties
     },
 
-    revalidate:60  //aqui define quanto tempo deve ser feita a requisição em getStaticProps
+    revalidate: 60  //aqui define quanto tempo deve ser feita a requisição em getStaticProps
   }
 }
 
